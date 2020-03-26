021043d1d1e3ae777107c95a45f80ab18dd79675
ceccs18c43 <60264386+ceccs18c43@users.noreply.github.com>
2020-03-26 13:16:22 +0530

    Add files via upload

---
+++ 2603/index.php
@@ -0,0 +1,14 @@
+<?php
+      
+ ECHO "<h1>Welcome....... </h1>";
+
+ ECHO "<p>Hai friends, I'm Nivea Gigen.</p>";
+ Echo "<h2>Let us learn PHP and have a fun</h2>";
+ 
+ Echo "<h3>ftp</h3>";
+ Echo "<p>You can ping me at 192.168.43.157 </p>";
+
+ Echo "<h3>contact me @</h3>";
+ Echo "<p>niveagen@gmail.com</p>";
+ 
+?> 
